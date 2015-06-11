6476d97f6c3c4fa8646fa034300a3a51b6e68656
Morgan Leonard <morganleonardpublic@gmail.com>
2015-06-11 10:40:15 -0700

    partially functioning info page router

--- routes/index.js
+++ routes/index.js
@@ -12,39 +12,50 @@ router.get('/', function(request, response) {
 router.post('/url', function(request, response) {
   var random = uuid.v4();
   var url = request.body.url;
-  var database = app.get('database');
-  var collection = db.collection('url_shortener');
-  collection.insert(
-    {
-      "shortened": random,
-      "target": url,
-      "clicks": 0
-    }, function(err, docs) {
-    collection.count(function(err, count) {
-      console.log("count = %s", count);
+
+  MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if (err) {
+      throw err;
+    }
+    var collection = db.collection('url_shortener');
+    collection.insert(
+      {
+        "shortened": random,
+        "target": url,
+        "clicks": 0
+      }, function(err, docs) {
+      collection.count(function(err, count) {
+        console.log("count = %s", count);
+      });
+      collection.update({'target': url}, { $inc: {"clicks": 1}
+      });
+      collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}
+      });
+      collection.find().toArray(function(err, results) {
+        //console.dir(results);
+        db.close();
+      });
     });
-    collection.update({'target': url}, { $inc: {"clicks": 1}
-    });
-    collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}
-    });
-    collection.find().toArray(function(err, results) {
-      console.dir(results);
-      db.close();
-    });
+    console.log(random);
+    console.log(url); 
+    response.redirect("/info/" + random);
   });
-  console.log(random);
-  console.log(url); 
-  response.redirect("/info/" + random);
 });
 
 //===================== GET handler for info page on short URL =============================//
 router.get('/info/:shortUrl', function(request, response) {
-  var database = app.get('database');
+  MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+    if (err) {
+      throw err;
+    }
     var collection = db.collection('url_shortener'),
         shortUrl = request.params.shortUrl;
-    collection.find().toArray({'shortened': shortUrl}, function(err, url) {
-      response.render('info', {url: url});
-    });
+    console.log ('short URL : ')
+    console.log (shortUrl);
+    collection.find({shortened : shortUrl}).toArray(function(err, results) {
+      console.log(results)
+      response.render('info', {url : results});
+      })
   });
 });
 
@@ -62,6 +73,3 @@ router.get('/:shortUrl', function(request, response) {
 
 
 module.exports = router;
-
-
-
