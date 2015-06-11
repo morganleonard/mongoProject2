39dce9040abe90219c50caa5a59016e66ed36307
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-11 14:59:23 -0700

    clicks and date is updated

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/', function(request, response) {
     }
     var collection = db.collection('url_shortener');
     console.log('inside mongo');
-    console.log(db.collection.find({target: "www.google.com"}));
+    // console.log(db.collection.find());
   });
   // console.log(collection.select({target}))
   response.render('index', {});
@@ -28,28 +28,42 @@ router.post('/url', function(request, response) {
       throw err;
     }
     var collection = db.collection('url_shortener');
-    collection.insert(
-      {
-        "shortened": random,
-        "target": url,
-        "clicks": 0,
-        "last_click" : null
-      }, function(err, docs) {
-      collection.count(function(err, count) {
-        console.log("count = %s", count);
-      });
-      collection.update({'target': url}, { $inc: {"clicks": 1}
-      });
-      collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}
-      });
-      collection.find().toArray(function(err, results) {
-        //console.dir(results);
-        db.close();
-      });
+    collection.find({"target": url}).toArray(function(err, results) {
+      console.log(results[0]);
+      if(results[0]){
+        collection.update({'target': url}, { $inc: {"clicks": 1}, 
+          $set: { "last_click": new Date()}
+        // });
+        // collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}
+        });
+        response.redirect("/info/" + results[0].shortened);
+      } else {
+        collection.insert(
+          {
+            "shortened": random,
+            "target": url,
+            "clicks": 0,
+            "last_click" : new Date()
+          }, function(err, docs) {
+              response.redirect("/info/" + random);
+              collection.count(function(err, count) {
+                console.log("count = %s", count);
+          });
+          // collection.update({'target': url}, { $inc: {"clicks": 1}
+          // });
+          // collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}
+          // });
+          // console.log(collection.find({"target": url}));
+          collection.find().toArray(function(err, results) {
+            console.dir(results);
+            db.close();
+          });
+        });
+
+      }
     });
     //console.log(random);
-    //console.log(url); 
-    response.redirect("/info/" + random);
+    //console.log(url);
   });
 });
 
@@ -64,7 +78,7 @@ router.get('/info/:shortUrl', function(request, response) {
     //console.log ('short URL : ')
     //console.log (shortUrl);
     collection.find({shortened : shortUrl}).toArray(function(err, results) {
-      //console.log(results[0])
+      console.log(results[0])
       response.render('info', {url : results[0]});
       })
   });
