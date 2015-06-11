7955ed9727c8e1b9e3577110fbacbddcbf58c50b
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-11 08:37:21 -0700

    found bug, needed closing arguments on get handler for info page on short url

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,16 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var uuid = require('node-uuid');
 
-
-
-
-
-
-
-
-
-
-
-
-
 //===================== GET handler for home page =============================//
 router.get('/', function(request, response) {
   response.render('index', {});
 });
-  // index.jade needs a form to submit a URL for shortening
-
-
-
-
-
-
 
 //===================== POST handler for submitting url in form on home page ======================//
 router.post('/url', function(request, response) {
   var random = uuid.v4();
   var url = request.body.url;
 
-  var group = {
-      "shortened": random,
-      "target": url,
-      "clicks": 8,
-      "last_click": "2015-01-13T16:42:00"
-  };
   MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
     if (err) {
       throw err;
@@ -52,8 +27,10 @@ router.post('/url', function(request, response) {
       collection.count(function(err, count) {
         console.log("count = %s", count);
       });
-      collection.update({'target': url}, { $inc: {"clicks": 1}});
-      collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}});
+      collection.update({'target': url}, { $inc: {"clicks": 1}
+      });
+      collection.update({'target': url}, { $currentDate: {"last_click": {$type: "timestamp"}}
+      });
       collection.find().toArray(function(err, results) {
         console.dir(results);
         db.close();
@@ -64,29 +41,6 @@ router.post('/url', function(request, response) {
     response.redirect("/info/" + random);
   });
 });
-  //create shortUrl and object with info like :
-  /*
-  {
-  "_id": "atnherjgrdkgdr",
-  "shortened": "bbb",
-  "target": "https://google.com",
-  "clicks": 8,
-  "last_click": "2015-01-13T16:42:00"
-}
-*/
-/*
-  var docToInsert = {
-    "shortened" : //create shortened url,
-    "target": url,
-    "clicks": 0,
-    "last_click": //current time
-  }
-  collection.insert({docToInsert}, function(err, docs) {
-    response.redirect('/info/' + shortUrl);
-  });
-});
-});
-*/
 
 //===================== GET handler for info page on short URL =============================//
 router.get('/info/:shortUrl', function(request, response) {
@@ -99,6 +53,7 @@ router.get('/info/:shortUrl', function(request, response) {
     collection.find().toArray({'shortened': shortUrl}, function(err, url) {
       response.render('info', {url: url});
     });
+  });
 });
 
 
